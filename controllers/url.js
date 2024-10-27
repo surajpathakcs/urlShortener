846700ee483991d5e6de1a6f9ccafac7efcbead9
npmzero <surajpathak3430@gmail.com>
2024-10-27 21:37:28 +0545

    Working delete/:id route and get/:id route to redirect to original url

--- controllers/url.js
+++ controllers/url.js
@@ -1,6 +1,10 @@
 const ShortID = require('ssid')
 const URL = require('../models/url');
 
+async function handleView(req,res) {
+    res.send(`<h1>APP Started</h1>`)
+}
+
 async function handleGenerateNewShortURL(req,res){
     const body = req.body;
     if(!body.url) return res.status(400).json({
@@ -23,21 +27,54 @@ async function handleGenerateNewShortURL(req,res){
 };
 
 async function handleGetOriginalURL(req,res){
-        const shortId= req.params.id
-        const urlEntry = await URL.findOne({shortID:shortId})
+        const Id= req.params.id
 
-        if(urlEntry) {
-           res.redirect(urlEntry.redirectURL)
+        try{
+            const urlEntry = await URL.findOne({shortID:Id})
+            if(urlEntry) {
+                return res.redirect(urlEntry.redirectURL)
+                console.log("redirect")
+            }
+            else{
+                res.status(404).json({
+                    error:"URL not found"
+                })
+            }
         }
-        else{
-            res.status(400).json({
-                error:"URL not found"
+        catch(error){
+            res.status(502).json({
+                error:"Something went wrong"
+            }
+            )
+        }
+}
+
+async function handleDeleteURL(req,res){
+    const id = req.params.id
+
+    try{        
+        const urlEntry = await URL.findOne({shortID:id})
+        if(!urlEntry){
+            return res.status(404).json({
+                error: "Entity Not Found"
             })
         }
-    
+        await URL.findOneAndDelete({shortID:id})
+        res.status(200).json({
+            message : "Deletion Success"
+        })
+    }
+    catch(err){
+        res.status(502).json({
+            error:"Something went wrong"
+        }
+        )
+    }
 }
 
 module.exports = {
+    handleView,
     handleGenerateNewShortURL,
-    handleGetOriginalURL
+    handleGetOriginalURL,
+    handleDeleteURL
 }
--- routes/url.js
+++ routes/url.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const {handleGenerateNewShortURL}  = require('../controllers/url')
+const {handleGenerateNewShortURL, handleDeleteURL, handleGetOriginalURL, handleView}  = require('../controllers/url')
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.send(`<h1>APP Started</h1>`)
-})
 
-
+router.get('/',handleView)
 router.post('/',handleGenerateNewShortURL);
+router.delete('/:id',handleDeleteURL);
+router.get('/:id',handleGetOriginalURL);
 
 module.exports = router;
